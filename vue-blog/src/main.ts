f11fdb89bf11ccefde9e46d436ffdfbb49d4b07b
Hideyuki KOBAYASHI <kobayashi.hideyuki@leadinge.co.jp>
2023-02-15 23:39:49 +0900

    update vuetifyインポートの記述をplugins/vuetify.tsにまとめた

--- vue-blog/src/main.ts
+++ vue-blog/src/main.ts
@@ -1,31 +1,17 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { vuetify3 } from "./plugins/vuetify";
 
 import App from "./App.vue";
 import router from "./router";
 
-// Vuetify 3
-// https://next.vuetifyjs.com/en/getting-started/installation/#manual-steps
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-import labs from "./plugins/vuetify";
-import "@mdi/font/css/materialdesignicons.css"; // この行を追加
-
 // assets
 import "./assets/main.css";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(vuetify);
-app.use(labs);
+app.use(vuetify3);
 
 app.mount("#app");
--- vue-blog/src/plugins/vuetify.ts
+++ vue-blog/src/plugins/vuetify.ts
@@ -3,11 +3,22 @@
  *
  * Framework documentation: https://vuetifyjs.com`
  */
+
+// Vuetify 3
+// https://next.vuetifyjs.com/en/getting-started/installation/#manual-steps
+import "@mdi/font/css/materialdesignicons.css"; // mdi font
+import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+// Vuetify Labs
+// https://vuetifyjs.com/en/labs/introduction/
 import * as labs from "vuetify/labs/components";
 
-export default createVuetify({
+export const vuetify3 = createVuetify({
   components: {
+    ...components,
     ...labs,
   },
+  directives,
 });
